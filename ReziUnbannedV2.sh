2189431faddfbd0ae3c9a921a074a1154c3805a6
Rezi-Xploit335 <remanrhn06@gmail.com>
2026-03-24 16:08:33 +0700

    Add files via upload

---
+++ ReziUnbannedV2.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+clear
+echo -e "\e[1;36mLOADING....."
+sleep 2
+echo -e "\e[1;32mBEHASIL MEMUAT SC"
+clear
+
+echo "█▀▀▄ █▀ ▀▀▀█ █ ░░ █░█ █▄░█ █▀▄ ▄▀▄ █▄░█ █▀▄"
+echo "█▐█▀ █▀ ░▄▀░ █ ▀▀ █░█ █░▀█ █▀█ █▀█ █░▀█ █░█"
+echo "▀░▀▀ ▀▀ ▀▀▀▀ ▀ ░░ ▀▀▀ ▀░░▀ ▀▀░ ▀░▀ ▀░░▀ ▀▀░"
+echo $bluelight"╭─────────────────────────────────────────╮"
+echo "│User status : PREMIUM                    │"
+echo "│─────────────────────────────────────────│"
+echo "│DEV : ReziXploit                         │"
+echo "│Telegram : @ReziXploit                   │"
+echo "│Tiktok : @rezixploitofficial             │"
+echo "│Channel tele : @AbtRezi                  │"
+echo "│─────────────────────────────────────────│"
+echo "│Tools name : REZI-UNBAND                 │"
+echo "│Version : V2.1                           │"
+echo "│─────────────────────────────────────────│"
+echo "│Spam sewajar nya!                        │"
+echo "│✅UNBANNED KENON SPAM/BIASA              │"
+echo "│🥶UNBANNED KENON MANEN FRESH             │"
+echo "│🔥UNBANNED KENON MANEN HARD              │"
+echo "│😈UNBANNED KENON MANEN BATU              │"
+echo "│🗿UNBANNED KENON MANEN LUMUT             │"
+echo "│langsung 8××× tidak pakai +62 atau 08××× │"
+echo "╰─────────────────────────────────────────╯"
+echo -e "\e[31;1m╭─────────────────────────────────────────"
+read -p "╰──>+62 :" nomor
+
+[ $nomor == android@support.whatsapp.com spam 839]:then
+    echo -e "\e[1;36mproses... █████░░░░░░░░░░░░░░░░░░ 10%"
+    sleep 2
+    echo "proses... █████████░░░░░░░░░░░░░░ 15%"
+    sleep 3
+    echo "proses... ██████████░░░░░░░░░░░░░ 20%"
+    sleep 4
+    echo "proses... █████████████░░░░░░░░░░ 30%"
+    sleep 5
+    echo "proses... ██████████████░░░░░░░░░ 50%"
+    sleep 6
+    echo "proses... █████████████████░░░░░░ 87%"
+    sleep 7
+    echo "proses... █████████████████████░░ 98%"
+    sleep 8
+    echo "proses... ██████████████████████░ 99%"
+    sleep 9
+    echo "proses... ███████████████████████ 100%"
+    sleep 10
+    echo -e "\e[1;32mDONE UNBAND by Rezi✅ ________________ 100%"
